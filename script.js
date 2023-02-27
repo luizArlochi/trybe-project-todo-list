ac020f59bf0fc5e8947ae5c2483c5879cfaa2c06
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-02-27 20:34:47 -0300

    requisito 13 concluido

--- script.js
+++ script.js
@@ -105,3 +105,46 @@ window.addEventListener('load', () => {
 });
 
 document.body.appendChild(buttonSaveTasks);
+
+const buttonMoveUp = document.getElementById('mover-cima');
+const buttonMoveDown = document.getElementById('mover-baixo');
+
+buttonMoveUp.addEventListener('click', () => {
+  if (selectedTask !== null && selectedTask.previousSibling !== null) {
+    const index = Array.from(taskList.children).indexOf(selectedTask);
+    const previousIndex = index - 1;
+    taskList.insertBefore(selectedTask, taskList.children[previousIndex]);
+    tasks.splice(index, 1);
+    tasks.splice(previousIndex, 0, {
+      task: selectedTask.innerText,
+      completed: selectedTask.classList.contains('completed'),
+    });
+  }
+});
+
+buttonMoveDown.addEventListener('click', () => {
+  if (selectedTask !== null && selectedTask.nextSibling !== null) {
+    const index = Array.from(taskList.children).indexOf(selectedTask);
+    const nextIndex = index + 1;
+    taskList.insertBefore(selectedTask.nextSibling, selectedTask);
+    tasks.splice(index, 1);
+    tasks.splice(nextIndex, 0, {
+      task: selectedTask.innerText,
+      completed: selectedTask.classList.contains('completed'),
+    });
+  }
+});
+
+document.body.appendChild(buttonMoveUp);
+document.body.appendChild(buttonMoveDown);
+
+const buttonRemoveSelected = document.getElementById('remover-selecionado');
+
+buttonRemoveSelected.addEventListener('click', () => {
+  if (selectedTask !== null) {
+    const index = tasks.findIndex((task) => task.task === selectedTask.innerText);
+    tasks.splice(index, 1);
+    selectedTask.remove();
+    selectedTask = null;
+  }
+});
