c0a3647d2be24728fb57dee99029baa507a3693e
Luiz Arlochi <luiz.arlochi@gmail.com>
2023-02-27 11:24:59 -0300

    requisitos 6 7 e 8 concluidos

--- script.js
+++ script.js
@@ -1,14 +1,45 @@
 const buttonCreateTask = document.getElementById('criar-tarefa');
 const textArea = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+let tasks = [];
+let selectedTask = null;
 
 buttonCreateTask.addEventListener('click', () => {
   const textTask = textArea.value;
 
   if (textTask !== '') {
-    const newTask = document.createElement('li');
-    newTask.innerText = textTask;
-    taskList.appendChild(newTask);
+    const newTask = {
+      task: textTask,
+      timestamp: Date.now(),
+      completed: false,
+    };
+    tasks.push(newTask);
+    tasks.sort((a, b) => a.timestamp - b.timestamp);
+    updateTaskList();
     textArea.value = '';
   }
 });
+
+function updateTaskList() {
+  taskList.innerHTML = '';
+  tasks.forEach((task) => {
+    const newTask = document.createElement('li');
+    newTask.innerText = task.task;
+    newTask.addEventListener('click', () => {
+      if (selectedTask !== null) {
+        selectedTask.style.backgroundColor = '';
+      }
+      newTask.style.backgroundColor = 'gray';
+      selectedTask = newTask;
+    });
+    newTask.addEventListener('dblclick', () => {
+      task.completed = !task.completed;
+      updateTaskList();
+    });
+    if (task.completed) {
+      newTask.classList.add('completed');
+    }
+    taskList.appendChild(newTask);
+  });
+}
+
